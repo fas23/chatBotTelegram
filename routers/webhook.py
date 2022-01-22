d0343b7b2c27b03ff18239aa3f0e64b20f13a4d1
fas23 <sajama230@gmail.com>
2022-01-22 09:45:37 -0300

    Agrego archivo de configuracion

--- routers/webhook.py
+++ routers/webhook.py
@@ -8,7 +8,6 @@ api = APIRouter(prefix="", tags=["webhook"])
 
 @api.post(path="/webhook", status_code=200)
 async def webhook(request: TelegramMessageSchema = Body(..., example=TelegramMessageSchema.get_example())):
-    print(request.message)
     text_response = await DialogFlowService.message(text=request.message.text)
     content, status_code = await BotService.reply_message(chat_id=request.message.chat.id, text=text_response)
     return Response(content="Message sent", status_code=status_code)
